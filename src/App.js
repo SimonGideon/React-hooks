25a194e0b66be92e894373a15d763516b62e3878
SimonGideon <simongideon918@gmail.com>
2022-12-22 23:55:41 +0300

    Rendering Objects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import './App.css';
 
-const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
 
 function App() {
   return (
     <div>
-      <h1>Hello World {title} </h1>
+      <h1>{welcome.greeting} {welcome.title} </h1>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
     </div>
