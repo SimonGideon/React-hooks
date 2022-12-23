9cf25129e8b31213e1a5626d5333c5de97e39594
SimonGideon <simongideon918@gmail.com>
2022-12-23 23:58:17 +0300

    Map-fucntion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import './App.css';
-const title = 'React';
+import React from "react";
+import "./App.css";
+const getTitle = () => 'React';
 
-const getTitle = () => title;
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abromov",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1> Hello {getTitle('React')} </h1>
+      <h1> Hello {getTitle("React")} </h1>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
+      {
+        list.map(function(item) {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          ) 
+        })
+      }
     </div>
   );
 }
