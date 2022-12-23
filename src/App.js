0d34a3afd288bc99f2c5ce623af4f8bec9298bbd
SimonGideon <simongideon918@gmail.com>
2022-12-24 02:14:06 +0300

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
 import "./App.css";
 const getTitle = () => "React";
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   const handleChange = event => {
