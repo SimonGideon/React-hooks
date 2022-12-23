eb5d377bc6110e9f2d34788baad7679890347a7a
SimonGideon <simongideon918@gmail.com>
2022-12-24 02:15:48 +0300

    Added List components

--- src/App.js
+++ src/App.js
@@ -30,7 +30,20 @@ function App() {
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" onChange={handleChange} />
       <hr />
+      <List />
     </div>
   );
 }
+
+const List = () =>
+  list.map((item) => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
 export default App;
