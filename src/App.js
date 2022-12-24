eb0a34fac34175e303a5ea1675907aac4e3e6544
SimonGideon <simongideon918@gmail.com>
2022-12-24 21:30:05 +0300

    initializing state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import React from "react";
 import "./App.css";
 const getTitle = () => "React";
-const stories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
 function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  const [searchTerm, setSearchTerm] = React.useState('');
   // handle change function
   const handleChange = event => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   }
   return (
     <div>
       <h1> Hello {getTitle("React")} </h1>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
       <hr />
       <List list={stories}/>
     </div>
